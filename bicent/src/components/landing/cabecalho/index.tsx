1d7a0120805eba2d3a3019807f3725b069f71b75
Daniel Ribeiro Skroski <daniel@duplod.com.br>
2023-05-18 06:53:08 -0300

    Landing Page Bicent

---
+++ bicent/src/components/landing/cabecalho/index.tsx
@@ -0,0 +1,17 @@
+import Area from "../comum/Area";
+import Logo from "../comum/Logo";
+import Menu from "./Menu";
+
+export default function Cabecalho() {
+    return(
+        <Area className="bg-black"> 
+            <div className="flex items-center justify-between h-20">
+                <Logo />
+
+                <Menu />
+            </div>
+        </Area>
+         
+    
+    )
+}
---
+++ bicent/src/components/template/Pagina.tsx
@@ -0,0 +1,16 @@
+interface PaginaProps {
+  externa?: boolean;
+  children?: any;
+  className?: string;
+}
+export default function Pagina(props: PaginaProps) {
+  return (
+    <div className={`
+            flex flex-col min-h-screen 
+            bg-gradient-to-r from-zinc-900 via-black to-zinc-600
+        ${props.className ?? ''} 
+        `}>
+      {props.children}
+    </div>
+  )
+}
